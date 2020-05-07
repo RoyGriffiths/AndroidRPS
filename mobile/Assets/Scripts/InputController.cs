991f0db1af61bc8a99fa5f48b291b66d8971a58d
Roy Griffiths <roy.ah.griffiths@gmail.com>
2020-05-07 14:07:00 +0100

    Initial commit

---
+++ mobile/Assets/Scripts/InputController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    private AnimationController animationController;
+    private GamePlayController gameplayController;
+
+    private string playersChoice;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animationController = GetComponent<AnimationController>();
+        gameplayController = GetComponent<GamePlayController>();
+    }
+
+    public void GetChoice(){
+
+        string choiceName = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+        print("Player selected: " + choiceName);
+
+        GameChoices selectedChoice = GameChoices.NONE;
+
+        switch(choiceName){
+
+            case "Rock":
+                selectedChoice = GameChoices.ROCK;
+                break;
+
+            case "Paper":
+                selectedChoice = GameChoices.PAPER;
+                break;
+
+            case "Scissors":
+                selectedChoice = GameChoices.SCISSORS;
+                break;
+        }
+
+        gameplayController.SetChoices(selectedChoice);
+        animationController.PlayerMadeChoice();
+    }
+}
